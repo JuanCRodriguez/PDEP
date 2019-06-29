832c0bb7414c724f7f9f20aa432c8f735e2db51e
Caterina Elizabeth Navarro <43158195+CaterinaNavarro@users.noreply.github.com>
2019-06-28 22:15:32 -0300

    Update democracia.pl

--- Prolog/TP 1/democracia.pl
+++ Prolog/TP 1/democracia.pl
@@ -1,9 +1,39 @@
-esCandidatoDelPartido(frank, rojo).
+sCandidatoDelPartido(frank, rojo).
 esCandidatoDelPartido(claire, rojo).
 esCandidatoDelPartido(garrett, azul).
+esCandidatoDelPartido(jackie, amarillo).
+esCandidatoDelPartido(linda, azul).
+esCandidatoDelPartido(catherine, rojo).
+esCandidatoDelPartido(seth, amarillo).
+esCandidatoDelPartido(heather, amarillo).
+
 
 edadDe(frank, 50).
 edadDe(claire, 52).
 edadDe(garret, 64).
+edadDe(jackie, 38).
+edadDe(peter, 26).
+edadDe(linda, 30).
+edadDe(catherine, 59).
+edadDe(heather, 50).
+
+
+habitantesDe(bsas, 15355000).
+habitantesDe(chaco, 1143201).
+habitantesDe(tierradelfuego, 60720).
+habitantesDe(sanluis, 489255).
+habitantesDe(neuquen, 637913).
+habitantesDe(santafe, 3397532).
+habitantesDe(cordoba, 3567654).
+habitantesDe(chubut, 577466).
+habitantesDe(formosa, 527895).
+habitantesDe(tucuman, 1687305).
+habitantesDe(salta, 1333365).
+habitantesDe(santacruz, 273964).
+habitantesDe(lapampa, 349299).
+habitantesDe(corrientes, 992595).
+habitantesDe(misiones, 1189446).
+
+
 
 
