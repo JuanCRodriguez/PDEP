47b1b8f97ea266628983a025699ebbad93ec5ecd
JuanCRodriguez <Rodriguez.JuanCarlos@outlook.com>
2019-11-26 14:04:22 -0300

    inicio un poco mas clojure

--- clojure/drcasa/src/drcasa/core.clj
+++ clojure/drcasa/src/drcasa/core.clj
@@ -1,6 +1,19 @@
-(ns drcasa.core)
+;(ns drcasa.core)
+
+(defprotocol Persona ;Funciona como si fuera una clase abstracta, muestro la firma de los metodos que van a implementar
+					 ; todo: seguir implementando
+	(aumentarTemperatura [self cantidad] update self :temperatura + cantidad))
+
+(defprotocol Enfermedad  
+	(esAgresiva [self persona])
+	(pasarDia [self persona]))
+	
 (defrecord Persona [nombre temperatura celulas enfermedades])
-(defrecord EnfermedadInfecciosa [nombre celulasAmenazadas])
+(defrecord EnfermedadInfecciosa [nombre celulasAmenazadas]
+	Enfermedad
+	(esAgresiva [self persona] (> celulasAmenazadas ((comp (partial * 0.10) :celulas) persona)))
+	(pasarDia [self persona] (aumentarTemperatura persona (/ 1000 celulasAmenazadas))))
+	
 (defrecord EnfermedadAutoInmune [nombre celulasAmenazadas cantDias])
 
 (def unaMalaria (->EnfermedadInfecciosa "Malaria" 500))
@@ -11,12 +24,3 @@
 (def logan (->Persona "Logan" 35 3000000 [unaMalaria otitis lupus]))
 (def frank (->Persona "Frank" 36 3500000 []))
 
-
-(defprotocol enfermedadEsAgresiva (fn [enfermedad persona] (class enfermedad)))
-(defmethod enfermedadEsAgresiva drcasa.core.EnfermedadInfecciosa
-  [enfermedad persona]
-  (> (:celulasAmenazadas enfermedad) ((comp (partial * 0.10) :celulas) persona)))
-;(defmethod enfermedadEsAgresiva :autoinmune
-;  [enfermedad persona]
-;  )
-;(def (enfermedadEsAgresiva unaMalaria logan))
