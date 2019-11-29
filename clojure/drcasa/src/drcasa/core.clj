5d2215a64c4e291a6364a822e679ec66b64e6a5d
JuanCRodriguez <Rodriguez.JuanCarlos@Outlook.com>
2019-11-28 23:28:21 -0300

    parte 1 terminada creo

--- clojure/drcasa/src/drcasa/core.clj
+++ clojure/drcasa/src/drcasa/core.clj
@@ -1,60 +1,111 @@
-;(ns drcasa.core)
-;(require '[clojure.core.reducers :as r])(
-;(require '[drcasa.core] :as i)
+(ns drcasa.core)
 
-(defn flip 
+(defn flip
+	"	Se comporta como el flip de haskell, lo defino a manopla porque no existe en clojure"
 	[f arg1, arg2]
 	(f arg2 arg1))
 
 (defprotocol Enfermedad
+	"	Protocolo base para las enfermedades
+	"
 	(esAgresiva [self persona])
 	(afectarPersona [self persona]))
 
 (defprotocol _EnfermedadInfecciosa
+	"Protocolo para las enfermedades infecciosas
+	"
 	(reproducirse [self]))
 
 (defprotocol _EnfermedadAutoInmune
+	"	Protocolo para las enfermedades autoinmunes
+	"
 	(aumentarDia [self]))
 
 
-(defn pasarDias [persona dias] (dotimes [n dias] ()))
-
 (defprotocol _Persona
+	"	Protocolo para las personas
+	"
 	(contraerEnfermedad [self enfermedad])
-	(pasarDia [self]))
+	(pasarDia [self])
+	(pasarDias [self n])
+	(aumentarTemperatura [self cuanto])
+	(disminuirCelulas [self cuanto])
+	(enComa [self])
+	(getEnfAutoInmunes [self]))
 
 (defrecord Persona [nombre temperatura celulas enfermedades]
+
 	_Persona
-	(contraerEnfermedad [self enfermedad] (update-in self [:enfermedades] conj enfermedad))
-	(pasarDia [self] (map (partial flip afectarPersona self) (:enfermedades self)))
-	(aumentarDiaEnfermedad [self enfermedades] )
-	)
+	(getEnfAutoInmunes [self]
+		"satisfies?: retorna true si el elemento implementa ese protocolo
+		"
+		(filter (partial satisfies? _EnfermedadAutoInmune) enfermedades))
 
-(defn aumentarTemperatura [persona cuanto] (update persona :temperatura + cuanto))
-(defn disminuirCelulas [persona cuanto] (update persona :celulas - cuanto))
+	(contraerEnfermedad [self enfermedad]
+		"	update-in: actualiza el valor de las keywords indicadas
+			conj: agrega un elemento a una lista
+		"
+		(update-in self [:enfermedades] conj enfermedad))
 
+	(enComa [self]
+		"	some: similar a cualquier any
+		"
+		(or (some (comp (partial >= 5) :cantDias) (getEnfAutoInmunes self)) (> temperatura 45) (< celulas 1000000)))
+
+	(pasarDia [self]
+		"	Aplica todas las enfermedades
+			reduce: funciona parecido al fold de haskell
+		"
+		(reduce (partial flip afectarPersona) self enfermedades))
+
+	(pasarDias [self n]
+		"	Pasan N dias.
+			nth: consigue el n elemento de una lista
+			iterate: genera una lista infinita del estilo f(f(x))
+			"
+		(nth (iterate pasarDia self) n))
+
+	(aumentarTemperatura
+		[self cuanto]
+		(update self :temperatura + cuanto))
+
+	(disminuirCelulas
+		[self cuanto]
+		(update self :celulas - cuanto)))
 
 (defrecord EnfermedadInfecciosa [nombre celulasAmenazadas]
+
 	Enfermedad
-	(esAgresiva [self persona] (> (:celulasAmenazadas self) ((comp (partial * 0.10) :celulas) persona)))
-	(afectarPersona [self persona] (aumentarTemperatura persona (/ 1000 (:celulasAmenazadas self))))
+	(esAgresiva [self persona]
+		(> (:celulasAmenazadas self) ((comp (partial * 0.10) :celulas) persona)))
+
+	(afectarPersona [self persona]
+		((comp (partial aumentarTemperatura persona) (partial / 1000) :celulasAmenazadas) self))
+
 	_EnfermedadInfecciosa
-	(reproducirse [self] (update self :celulasAmenazadas * 2)))
+	(reproducirse [self]
+		(update self :celulasAmenazadas * 2)))
 	
 (defrecord EnfermedadAutoInmune [nombre celulasAmenazadas cantDias]
+
 	Enfermedad
-	(esAgresiva [_ _] (>= cantDias 30))
-	(afectarPersona [self persona] ((comp(partial disminuirCelulas persona) :celulasAmenazadas aumentarDia) self))
+	(esAgresiva [_ _]
+		(>= cantDias 30))
+
+	(afectarPersona [self persona]
+		((comp(partial disminuirCelulas persona) :celulasAmenazadas aumentarDia) self))
+
 	_EnfermedadAutoInmune
-	(aumentarDia [self] (update self :cantDias + 1)))
+	(aumentarDia [self]
+		(update self :cantDias + 1)))
+
+
 
 (def unaMalaria (->EnfermedadInfecciosa "Malaria" 500))
 (def otraMalaria (->EnfermedadInfecciosa "Malaria" 800))
 (def otitis (->EnfermedadInfecciosa "Otitis"  100))
-(def lupus (->EnfermedadAutoInmune "Lupus" 10000 0))
+(def lupus (->EnfermedadAutoInmune "Lupus" 10000 1))
 
 
 (def logan (->Persona "Logan" 35 3000000 [unaMalaria otitis lupus]))
 (def frank (->Persona "Frank" 36 3500000 []))
-
-(filter (fn [x] (instance? EnfermedadAutoInmune x)) (:enfermedades logan))
