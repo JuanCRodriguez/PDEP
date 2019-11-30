c02823939b44e1b9ab11a01201ca5e0ed0b5fb24
JuanCRodriguez <Rodriguez.JuanCarlos@Outlook.com>
2019-11-30 01:46:48 -0300

    v2 y tests terminados

--- clojure/drcasa/src/drcasa/core.clj
+++ clojure/drcasa/src/drcasa/core.clj
@@ -1,111 +1,225 @@
 (ns drcasa.core)
 
 (defn flip
-	"	Se comporta como el flip de haskell, lo defino a manopla porque no existe en clojure"
-	[f arg1, arg2]
-	(f arg2 arg1))
+  "	Se comporta como el flip de haskell, lo defino a manopla porque no existe en clojure"
+  [f arg1, arg2]
+  (f arg2 arg1))
 
 (defprotocol Enfermedad
-	"	Protocolo base para las enfermedades
-	"
-	(esAgresiva [self persona])
-	(afectarPersona [self persona]))
+  "	Protocolo base para las enfermedades
+  "
+  (esAgresiva [self persona])
+  (afectarPersona [self persona])
+  (disminuirCelulasAmenazadas [self cantidad]))
 
 (defprotocol _EnfermedadInfecciosa
-	"Protocolo para las enfermedades infecciosas
-	"
-	(reproducirse [self]))
+  "Protocolo para las enfermedades infecciosas
+  "
+  (reproducirse [self]))
 
 (defprotocol _EnfermedadAutoInmune
-	"	Protocolo para las enfermedades autoinmunes
-	"
-	(aumentarDia [self]))
+  "	Protocolo para las enfermedades autoinmunes
+  "
+  (aumentarDia [self]))
 
+"Defino las funciones que se van a comportar de igual manera en medico y en persona fuera de los records para no repetir logica"
+
+(defn _aumentarDiaEnfermedades
+  [self]
+  "	update-in data [:key] f: actualiza el valor aplicando f(:key data)
+    (# f arg): f(%) (en este caso el arg seria el valor viejo de update-in)
+    if condicion funcionEsTrue funcionEsFalse
+    utilizo map ya que :enfermedades retorna una lista
+  "
+  (update-in self [:enfermedades] (partial map #(if (satisfies? _EnfermedadAutoInmune %) (aumentarDia %) %))))
+
+(defn _contraerEnfermedad
+  "
+    lista conj elemento: agrega un elemento a una lista
+  "
+  [self enfermedad]
+  (update-in self [:enfermedades] conj enfermedad))
+
+(defn _pasarDia
+  "	Aplica todas las enfermedades
+    reduce f valorInicial Lista: funciona parecido al fold de haskell
+  "
+  [self]
+  ((comp _aumentarDiaEnfermedades (partial reduce (partial flip afectarPersona) self) :enfermedades) self))
+
+(defn _pasarDias
+  "
+    nth lista n: consigue el n elemento de una lista
+    iterate f: genera una lista infinita del estilo f(f(x))
+  "
+  [self n]
+  (nth (iterate _pasarDia self) n))
+
+(defn _aumentarTemperatura
+  [self cuanto]
+  (update self :temperatura + cuanto))
+
+(defn _disminuirCelulas
+  [self cuanto]
+  (update self :celulas - cuanto))
+
+(defn _getEnfAutoInmunes
+  " satisfies? protocolo elemento: retorna true si el elemento implementa ese protocolo
+  "
+  [self]
+  (filter (partial satisfies? _EnfermedadAutoInmune) (:enfermedades self)))
+
+(defn _compararTemp
+  " Recibe una funcion que espera un valor a comparar, ej: (partial > 5)
+  "
+  [self comparacion]
+  ((comp comparacion :temperatura) self))
+
+(defn _compararCelulas
+  " idem _compararTemp
+  "
+  [self comparacion]
+  ((comp comparacion :celulas) self))
+
+(defn _enfMayorA5Dias
+  ;Solo existe not-any? en clojure
+  ;asique tengo que usar not para hacer doble negacion ¯\\_(ツ)_/¯
+  ;some f lista: busca el primer elemento que cumpla la condicion, caso contrario retorna nil
+  ;not bool: (not true) = false
+
+  [self]
+  ((comp not (partial not-any? (comp (partial <= 5) :cantDias)) _getEnfAutoInmunes) self))
+
+(defn _enComa?
+  [self]
+  (or (_compararTemp self (partial <= 45)) (_compararCelulas self (partial >= 1000000)) (_enfMayorA5Dias self)))
+
+(defn _eliminarEnfermedadesCuradas [enfermedades]
+	(filter #((comp (partial < 0) :celulasAmenazadas) %) enfermedades))
+
+
+
+(defn _atenuarEnfermedades
+  [self cantidad]
+  (update-in self [:enfermedades]
+						 					(comp
+											_eliminarEnfermedadesCuradas
+											(partial map #(disminuirCelulasAmenazadas % cantidad)))))
+
+; La implementacion de un protocolo se puede realizar de dos maneras,
+; una es implementando en cada record y otra es generando un map con la implementacion de cada una y extendiendo el record
+; Para evitar la repeticion de logica y codigo, extiendo el record de Persona y Medico
+(def mapImplementacionesPersona
+  {:contraerEnfermedad
+   (fn [self enfermedad] (_contraerEnfermedad self enfermedad))
+   :pasarDia
+   (fn [self] (_pasarDia self))
+   :pasarDias
+   (fn [self n] (_pasarDias self n))
+   :aumentarTemperatura
+   (fn [self cuanto] (_aumentarTemperatura self cuanto))
+   :disminuirCelulas
+   (fn [self cuanto] (_disminuirCelulas self cuanto))
+   :enComa?
+   (fn [self] (_enComa? self))
+   :getEnfAutoInmunes
+   (fn [self] (_getEnfAutoInmunes self))
+	 :atenuarEnfermedades
+	 (fn [self cantidad] (_atenuarEnfermedades self cantidad))
+   })
 
 (defprotocol _Persona
-	"	Protocolo para las personas
-	"
-	(contraerEnfermedad [self enfermedad])
-	(pasarDia [self])
-	(pasarDias [self n])
-	(aumentarTemperatura [self cuanto])
-	(disminuirCelulas [self cuanto])
-	(enComa [self])
-	(getEnfAutoInmunes [self]))
+  "	Protocolo para las personas
+  "
+  (contraerEnfermedad [self enfermedad])
+  (pasarDia [self])
+  (pasarDias [self n])
+  (aumentarTemperatura [self cuanto])
+  (disminuirCelulas [self cuanto])
+  (enComa? [self])
+  (getEnfAutoInmunes [self])
+  (atenuarEnfermedades [self cantidad]))
 
-(defrecord Persona [nombre temperatura celulas enfermedades]
+(defprotocol _Medico
+  " Protocolo para los medicos
+  "
+  (calcularAtenuacion [self])
+  (curar [self, quien])
+	(curarse [[self]])
+  )
 
+(defprotocol _jefeMedico
+	(subordinadoRandom [self]))
+
+(defrecord Medico [nombre temperatura celulas enfermedades medicacion]
+  _Medico
+  (calcularAtenuacion [self] (* medicacion 15))
+	(curar [self quien] ((comp (partial atenuarEnfermedades quien) calcularAtenuacion) self))
+	(curarse [self] (curar self self)))
+
+(extend Medico
+	;Extiendo el record para implementar el protocolo persona
+  _Persona
+  mapImplementacionesPersona
+  )
+
+(defrecord JefeMedico [nombre temperatura celulas enfermedades subordinados]
+	_Medico
+	(curar [self quien] (curar (subordinadoRandom self) quien))
+	_jefeMedico
+	(subordinadoRandom [self] ((comp rand-nth :subordinados) self))
+	)
+(extend JefeMedico
 	_Persona
-	(getEnfAutoInmunes [self]
-		"satisfies?: retorna true si el elemento implementa ese protocolo
-		"
-		(filter (partial satisfies? _EnfermedadAutoInmune) enfermedades))
+	mapImplementacionesPersona)
 
-	(contraerEnfermedad [self enfermedad]
-		"	update-in: actualiza el valor de las keywords indicadas
-			conj: agrega un elemento a una lista
-		"
-		(update-in self [:enfermedades] conj enfermedad))
 
-	(enComa [self]
-		"	some: similar a cualquier any
-		"
-		(or (some (comp (partial >= 5) :cantDias) (getEnfAutoInmunes self)) (> temperatura 45) (< celulas 1000000)))
+(defrecord Persona [nombre temperatura celulas enfermedades])
+(extend Persona
+  _Persona
+  mapImplementacionesPersona)
 
-	(pasarDia [self]
-		"	Aplica todas las enfermedades
-			reduce: funciona parecido al fold de haskell
-		"
-		(reduce (partial flip afectarPersona) self enfermedades))
-
-	(pasarDias [self n]
-		"	Pasan N dias.
-			nth: consigue el n elemento de una lista
-			iterate: genera una lista infinita del estilo f(f(x))
-			"
-		(nth (iterate pasarDia self) n))
-
-	(aumentarTemperatura
-		[self cuanto]
-		(update self :temperatura + cuanto))
-
-	(disminuirCelulas
-		[self cuanto]
-		(update self :celulas - cuanto)))
-
+(defn _disminuirCelulasAmenazadas [self cantidad] (update self :celulasAmenazadas - cantidad))
 (defrecord EnfermedadInfecciosa [nombre celulasAmenazadas]
 
-	Enfermedad
-	(esAgresiva [self persona]
-		(> (:celulasAmenazadas self) ((comp (partial * 0.10) :celulas) persona)))
+  Enfermedad
+  (esAgresiva [self persona]
+    (> (:celulasAmenazadas self) ((comp (partial * 0.10) :celulas) persona)))
 
-	(afectarPersona [self persona]
-		((comp (partial aumentarTemperatura persona) (partial / 1000) :celulasAmenazadas) self))
+  (disminuirCelulasAmenazadas [self cantidad] (_disminuirCelulasAmenazadas self cantidad))
 
-	_EnfermedadInfecciosa
-	(reproducirse [self]
-		(update self :celulasAmenazadas * 2)))
-	
+  (afectarPersona [self persona]
+    ((comp (partial aumentarTemperatura persona) (partial / 1000) :celulasAmenazadas) self))
+
+  _EnfermedadInfecciosa
+  (reproducirse [self]
+    (update self :celulasAmenazadas * 2)))
+
 (defrecord EnfermedadAutoInmune [nombre celulasAmenazadas cantDias]
 
-	Enfermedad
-	(esAgresiva [_ _]
-		(>= cantDias 30))
+  Enfermedad
+  (esAgresiva [_ _]
+    (>= cantDias 30))
 
-	(afectarPersona [self persona]
-		((comp(partial disminuirCelulas persona) :celulasAmenazadas aumentarDia) self))
+  (disminuirCelulasAmenazadas [self cantidad] (_disminuirCelulasAmenazadas self cantidad))
 
-	_EnfermedadAutoInmune
-	(aumentarDia [self]
-		(update self :cantDias + 1)))
+  (afectarPersona [self persona]
+    ((comp (partial disminuirCelulas persona) :celulasAmenazadas aumentarDia) self))
+
+  _EnfermedadAutoInmune
+  (aumentarDia [self]
+    (update self :cantDias + 1)))
 
 
 
 (def unaMalaria (->EnfermedadInfecciosa "Malaria" 500))
 (def otraMalaria (->EnfermedadInfecciosa "Malaria" 800))
-(def otitis (->EnfermedadInfecciosa "Otitis"  100))
-(def lupus (->EnfermedadAutoInmune "Lupus" 10000 1))
+(def otitis (->EnfermedadInfecciosa "Otitis" 100))
+(def lupus (->EnfermedadAutoInmune "Lupus" 10000 0))
 
 
-(def logan (->Persona "Logan" 35 3000000 [unaMalaria otitis lupus]))
+(def logan (->Persona "Logan" 36 3000000 [unaMalaria otitis lupus]))
 (def frank (->Persona "Frank" 36 3500000 []))
+(def unMedico (->Medico "Pepe" 36 3500000 [] 2))
+(def otroMedico (->Medico "Nacho" 36 3500000 [] 40))
+(def house (->JefeMedico "House" 36 350000 [unaMalaria] [unMedico]))
--- clojure/drcasa/test/drcasa/core_test.clj
+++ clojure/drcasa/test/drcasa/core_test.clj
@@ -2,6 +2,57 @@
   (:require [clojure.test :refer :all]
             [drcasa.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-001
+  (testing "La malaria de 800 celulas se reproduce"
+    (is (= 1600 ((comp :celulasAmenazadas reproducirse) otraMalaria)))))
+
+(deftest test-002
+  (testing "Frank contrae la malaria de 800 celulas")
+  (let [_frank (contraerEnfermedad frank otraMalaria) _malaria ((comp first :enfermedades) _frank)]
+    (is (identical? otraMalaria _malaria)
+    (is (= 800 (:celulasAmenazadas _malaria))))))
+
+(deftest test-003
+  (testing "Logan vive un dia en su vida")
+  (let [_logan (pasarDia logan) _enfermedades (:enfermedades _logan)]
+    (is (= 48 (:temperatura _logan)))
+    (is (= 2990000 (:celulas _logan)))
+    (is (every? (comp (partial = 1) :cantDias) (getEnfAutoInmunes _logan)))
+    (is (enComa? _logan))))
+
+(deftest test-004
+  (testing "Frank contrae lupus y pasan 5 dias")
+  (let [_frank (pasarDias(contraerEnfermedad frank lupus) 5)]
+    (is (= 3450000 (:celulas _frank)))
+    (is (= 5 ((comp :cantDias first :enfermedades) _frank)))
+    (is (enComa? _frank))))
+
+(deftest test-005
+  (testing "un medico tiene una dosis de medicamento de 30")
+    (is (= 30 (calcularAtenuacion unMedico)))
+  )
+
+(deftest test-006
+  (testing "frank contrae malaria y unMedico se la atenua")
+  (let [_frank ((comp (partial curar unMedico) contraerEnfermedad) frank unaMalaria)]
+    (is (= 470 ((comp :celulasAmenazadas first :enfermedades) _frank)))))
+
+(deftest test-007
+  (testing "otroMedico cura todas las enfermedades de logan menos la lupus")
+  (let [_logan (curar otroMedico logan) _lupus ((comp first :enfermedades) _logan)]
+    (is (= "Lupus" (:nombre _lupus)))
+    (is (= 9400 (:celulasAmenazadas _lupus))))
+  )
+
+(deftest test-008
+  (testing "House envia un subordinado a atenuar la otitis de frank")
+  (let [_frank (curar house (contraerEnfermedad frank otitis))]
+    (is (= 70 ((comp :celulasAmenazadas first :enfermedades) _frank))))
+  )
+
+(deftest test-009)
+  (testing "un medico contrae Malaria y se la atenua a si mismo")
+  (let [_unMedico (curarse (contraerEnfermedad unMedico otraMalaria))]
+    (is (= 770 ((comp :celulasAmenazadas first :enfermedades) _unMedico))))
+
+(run-tests 'drcasa.core-test)
